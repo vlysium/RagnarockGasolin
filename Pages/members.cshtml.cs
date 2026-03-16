0eaabd3b6d5c87a8b9e25786e9c1e476773e1d4b
SAM-bits <saifatyaif@gmail.com>
2026-03-16 10:47:57 +0100

    members added

---
+++ Pages/members.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RagnarockGasolin.Pages
+{
+    public class membersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
