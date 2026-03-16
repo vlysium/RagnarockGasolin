28f35030e94e3b29a5c526f644bc7a12e89d18bf
Victor Ly <ly.victor2000@gmail.com>
2026-03-16 14:17:52 +0100

    qr codes

---
+++ Pages/KimLarsen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RagnarockGasolin.Pages
+{
+    public class KimLarsenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
