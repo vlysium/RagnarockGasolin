b991f851118ac24bec0d8c548420672fdf006400
Victor Ly <ly.victor2000@gmail.com>
2026-03-18 14:07:24 +0100

    small adjustment

--- Models/BandMember.cs
+++ Models/BandMember.cs
@@ -6,10 +6,7 @@ namespace RagnarockGasolin.Models {
         private string _href;
         private string _personImgSrc;
         private string _qrCodeImgSrc;
-        public string Name { get => _name; }
-        public string Href { get => _href; }
-        public string PersonImgSrc { get => _personImgSrc; }
-        public string QrCodeImgSrc { get => _qrCodeImgSrc; }
+        
         public BandMember(string name, string href, string personImgSrc, string qrCodeImgSrc)
         {
             _name = name;
@@ -17,5 +14,10 @@ namespace RagnarockGasolin.Models {
             _personImgSrc = personImgSrc;
             _qrCodeImgSrc = qrCodeImgSrc;
         }
+
+        public string Name { get => _name; }
+        public string Href { get => _href; }
+        public string PersonImgSrc { get => _personImgSrc; }
+        public string QrCodeImgSrc { get => _qrCodeImgSrc; }
     }
 }
