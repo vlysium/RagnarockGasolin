4d3eaae07feafbdd7f95197abcd4f97e8731931b
Victor Ly <ly.victor2000@gmail.com>
2026-03-19 12:05:49 +0100

    audio functionality

--- wwwroot/js/audio.js
+++ wwwroot/js/audio.js
@@ -1,10 +1,15 @@
 const musicButtons = document.querySelectorAll(".music-button");
+const audioFiles = document.querySelectorAll(".audio-file");
 
 musicButtons.forEach(button => button.addEventListener("click", () => {
   const audioId = button.dataset.id;
   const audioFile = document.getElementById(audioId);
 
   if (audioFile.paused) {
+    // pause all other audios
+    audioFiles.forEach(audio => audio.pause());
+    musicButtons.forEach(button => button.classList.remove("playing"));
+
     audioFile.play();
     button.classList.add("playing");
   } else {
