493080a9e27d2f662f0997677d817c1af5eb23b8
Victor Ly <ly.victor2000@gmail.com>
2026-03-19 11:41:31 +0100

    audio functionality

--- wwwroot/js/audio.js
+++ wwwroot/js/audio.js
@@ -1,9 +1,14 @@
-  const audio = document.getElementById('kvindeMin');
-  
-  function toggleMusic() {
-    if (audio.paused) {
-      audio.play();
-    } else {
-      audio.pause();
-    }
+const musicButtons = document.querySelectorAll(".music-button");
+
+musicButtons.forEach(button => button.addEventListener("click", () => {
+  const audioId = button.dataset.id;
+  const audioFile = document.getElementById(audioId);
+
+  if (audioFile.paused) {
+    audioFile.play();
+    button.classList.add("playing");
+  } else {
+    audioFile.pause();
+    button.classList.remove("playing");
   }
+}));
