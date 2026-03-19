978bbd203e888daed91ebbaca798aeeae0058e2b
rchmeis <r.chmeis@gmail.com>
2026-03-19 18:22:16 +0100

    rettet i danske tekster - mere læsbare og engagerende

---
+++ wwwroot/Elena.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RagnarockGasolin.wwwroot
+{
+    public class ElenaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
